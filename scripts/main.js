52b9fb21d5906631fc7db4bd835c43b94a75c74e
Ryan <32826618+DeltaEpiales@users.noreply.github.com>
2023-08-28 17:30:53 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the hero section for the interactive background
+    const heroSection = document.querySelector(".hero");
+    const heroContent = document.querySelector(".hero-content");
+
+    // Add event listeners for mouse movement
+    heroSection.addEventListener("mousemove", function(e) {
+        const moveX = (e.clientX - window.innerWidth / 2) / 10;
+        const moveY = (e.clientY - window.innerHeight / 2) / 10;
+        
+        // Apply parallax effect to the hero content
+        heroContent.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    });
+
+    // Reset the hero content position on mouse leave
+    heroSection.addEventListener("mouseleave", function() {
+        heroContent.style.transform = "translate(0, 0)";
+    });
+});
