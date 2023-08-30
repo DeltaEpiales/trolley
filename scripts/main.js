00a3038a6c9f6399af08c890e2201fc26036f542
Ryan <32826618+DeltaEpiales@users.noreply.github.com>
2023-08-29 17:13:04 -0700

    Add files via upload
    Visual improvements, mobile improvements, new menu style, new color scheme

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const trolleyImage = document.querySelector(".trolley-image");
-    const ribbon = document.querySelector(".ribbon");
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+  const mobileMenu = document.querySelector('.mobile-menu');
 
-    const trolleyStartPosition = {
-        x: trolleyImage.offsetLeft,
-        y: trolleyImage.offsetTop
-    };
-
-    document.addEventListener("mousemove", function(e) {
-        const moveX = (e.clientX - window.innerWidth / 2) / 20;
-        const moveY = (e.clientY - window.innerHeight / 2) / 20;
-
-        const newX = trolleyStartPosition.x + moveX;
-        const newY = trolleyStartPosition.y + moveY;
-
-        trolleyImage.style.transform = `translate(${newX}px, ${newY}px)`;
-        ribbon.style.transform = `translateX(${moveX}px)`;
-    });
-
-    document.addEventListener("mouseleave", function() {
-        trolleyImage.style.transform = `translate(${trolleyStartPosition.x}px, ${trolleyStartPosition.y}px)`;
-        ribbon.style.transform = "translateX(0)";
-    });
+  mobileMenuIcon.addEventListener('click', function () {
+    mobileMenu.classList.toggle('active');
+    mobileMenuIcon.classList.toggle('open'); // Toggle 'open' class on icon
+  });
 });
